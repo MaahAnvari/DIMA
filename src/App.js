9a08dffb18ba62eca650dd30e9d61050f2b6d62e
Tommaso Pozzi <tommaso4.pozzi@mail.polimi.it>
2022-05-09 16:18:32 +0200

    Removed navigation

--- src/App.js
+++ src/App.js
@@ -25,22 +25,3 @@ const App = () => {
 };
 
 export default App;
-
-/*
-import React from 'react';
-
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-import AppStack from './navigation/AppStack';
-
-function App() {
-  return (
-    <NavigationContainer>
-      <AppStack />
-    </NavigationContainer>
-  );
-}
-
-export default App;
-*/
