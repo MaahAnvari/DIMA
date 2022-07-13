b6b9ccb2c035d1a3767243fb24dd1237a9f98a4c
Tommaso Pozzi <tommaso4.pozzi@mail.polimi.it>
2022-07-13 20:13:12 +0200

    Test

--- __tests__/HomePage.test.js
+++ __tests__/HomePage.test.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import {render} from '@testing-library/react-native';
+import {render, fireEvent} from '@testing-library/react-native';
 
-const HomePage = require('../src/components/HomePage');
+import HomePage from '../src/components/HomePage';
 
 describe('Home Page', () => {
   it('Go to AboutPage', () => {
     const page = render(<HomePage />);
 
     const aboutButton = page.getByTestId('aboutButton');
+
+    fireEvent.press(aboutButton);
   });
 });
